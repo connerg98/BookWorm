fa47e4e1a7aaef67707b4ad0f8107f791440299d
Conner Glasgow <connerg38@gmail.com>
2022-04-15 16:30:41 -0700

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Conner Glasgow on 4/15/22.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
